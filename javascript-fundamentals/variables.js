efa88dd8d8db566c8d634f704c628ca2cbeeabbc
padovese <bhspadoveze@gmail.com>
2018-06-20 22:37:18 -0300

    initial commit

---
+++ javascript-fundamentals/variables.js
@@ -0,0 +1,3 @@
+var a;              //variable old mode, not recommended to use
+let b;              //variablenew mode, take best practices and is recommended to use
+const c;            //constants
