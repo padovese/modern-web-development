939595c524e64cc5463e9d93ef092fe03babbc0e
padovese <bhspadoveze@gmail.com>
2018-06-22 21:06:10 -0300

    declaring objects in two distincts forms

---
+++ javascript-fundamentals/objects.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.name = 'Some name';
+prod1.price = 10.99;
+prod1.amount = 3;
+console.log(prod1);
+
+
+const prod2 ={
+    name: 'some name 2',
+    price: 99.99,
+    amount: 10
+}
+console.log(prod2);
